7903da6ae8c0624eacebeb2d452a75ae4ca518de
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2026-08-13 01:21:22 -0500

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -8,6 +8,6 @@ class Solution:
         z = np.dot(x, w) + b
         y_hat = 1 / (1 + np.exp(-z))
         diff = y_hat - y_true
-        L = np.square(diff) / 2.0
-        dL_dw, dL_db = diff * y_hat * (1 - y_hat) * x, diff * y_hat * (1 - y_hat)
-        return (np.round(dL_dw, 5), round(dL_db, 5))
+        sigmoid_deriv = y_hat * (1 - y_hat)
+        delta = diff * sigmoid_deriv
+        return (np.round(delta * x, 5), round(delta, 5))
