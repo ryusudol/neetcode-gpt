94fe6f178d3009e3b0f3ee568cb78766dc348826
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2026-08-17 00:10:42 -0500

    Update MLP from scratch (Build a Neural Net 4/5)

--- foundations/mlp.py
+++ foundations/mlp.py
@@ -5,8 +5,7 @@ from typing import List
 
 class Solution:
     def forward(self, x: NDArray[np.float64], weights: List[NDArray[np.float64]], biases: List[NDArray[np.float64]]) -> NDArray[np.float64]:
-        for i in range(len(weights)):
-            x = weights[i].T @ x + biases[i]
-            x = np.maximum(0.0, x)
-        
+        for idx, (weight, bias) in enumerate(zip(weights, biases)):
+            x = np.dot(x, weight) + bias
+            x = np.maximum(0.0, x) if idx != len(weights) - 1 else x
         return np.round(x, 5)
