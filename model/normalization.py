6caae96b9cad9831861cb8577e62f44f7fd9b180
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2026-08-20 13:42:06 -0500

    Update Layer normalization (PyTorch 2/4)

--- model/normalization.py
+++ model/normalization.py
@@ -4,10 +4,6 @@ from numpy.typing import NDArray
 
 class Solution:
     def forward(self, x: NDArray[np.float64], gamma: NDArray[np.float64], beta: NDArray[np.float64]) -> NDArray[np.float64]:
-        mean = np.mean(x)
-        var = np.mean((x - mean) ** 2)
-        eps = 1e-5
-
-        x_hat = (x - mean) / np.sqrt(var + eps) * gamma + beta
-
-        return np.round(x_hat, 5)
+        avg = np.mean(x)
+        var = np.mean((x - avg) ** 2)
+        return np.round((x - avg) / np.sqrt(var + 1e-5) * gamma + beta, 5)
