babeed7623a6790be85462893a96c7f4aec646c1
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2026-08-22 11:56:11 -0500

    Update NLP preprocessing (NLP 2/4)

--- data/nlp_preprocessing.py
+++ data/nlp_preprocessing.py
@@ -5,31 +5,36 @@ from typing import List
 
 class Solution:
     def get_dataset(self, positive: List[str], negative: List[str]) -> TensorType[float]:
-        voca = set()
-        for (p_sent, n_sent) in zip(positive, negative):
-            for word in p_sent.split(): voca.add(word)
-            for word in n_sent.split(): voca.add(word)
-        voca = { item: idx + 1 for idx, item in enumerate(sorted(voca)) }
+        # voca = set()
+        # for (p_sent, n_sent) in zip(positive, negative):
+        #     for word in p_sent.split(): voca.add(word)
+        #     for word in n_sent.split(): voca.add(word)
+        # voca = { item: idx + 1 for idx, item in enumerate(sorted(voca)) }
 
-        encodings, max_len = [], 0
-        for sent in positive:
-            enc = []
-            for word in sent.split(): enc.append(voca[word])
-            encodings.append(enc.copy())
-            max_len = max(max_len, len(enc))
-        for sent in negative:
-            enc = []
-            for word in sent.split(): enc.append(voca[word])
-            encodings.append(enc.copy())
-            max_len = max(max_len, len(enc))
+        # encodings, max_len = [], 0
+        # for sent in positive:
+        #     enc = []
+        #     for word in sent.split(): enc.append(voca[word])
+        #     encodings.append(enc.copy())
+        #     max_len = max(max_len, len(enc))
+        # for sent in negative:
+        #     enc = []
+        #     for word in sent.split(): enc.append(voca[word])
+        #     encodings.append(enc.copy())
+        #     max_len = max(max_len, len(enc))
         
-        for enc in encodings:
-            if len(enc) < max_len:
-                while len(enc) < max_len:
-                    enc.append(0.0)
+        # for enc in encodings:
+        #     if len(enc) < max_len:
+        #         while len(enc) < max_len:
+        #             enc.append(0.0)
         
-        # nn.utils.rnn.pad_sequence(torch.tensor(encodings))
+        # return encodings
 
-        return encodings
+        combined = positive + negative
 
-        # 4. Pad shorter sequences with 0s using nn.utils.rnn.pad_sequence(tensors, batch_first=True)
+        voca = sorted({word for sentence in combined for word in sentence.split()})
+        word_to_id = { word: idx + 1 for idx, word in enumerate(voca) }
+
+        encoded = [torch.tensor([word_to_id[w] for w in s.split()]) for s in combined]
+
+        return nn.utils.rnn.pad_sequence(encoded, batch_first=True)
